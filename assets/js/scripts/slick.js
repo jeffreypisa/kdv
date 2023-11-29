7195098c445d3623925c0fa76153da1eb4693bb4
Jeffrey Pisa <jeffrey.pisa@gmail.com>
2023-11-29 15:16:04 +0100

    header fix

--- assets/js/scripts/slick.js
+++ assets/js/scripts/slick.js
@@ -2,21 +2,6 @@ import $ from "jquery";
 import 'slick-carousel';
 
 export function slick_init() {	
-	
-	$('.js-slick-sliderblok').slick({
-		infinite: true,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		arrows: true,
-		dots: false,
-		centerMode: true,
-		fade: true,
-		autoplay: true,
-		swipeToSlide: true,
-		speed: 2000,
-		cssEase: 'cubic-bezier(.19,1,.22,1)'
-	});
-	
 	$('.slick-slider').slick({
 		slidesToShow: 3,
 		responsive: [
@@ -34,6 +19,22 @@ export function slick_init() {
 			}
 		]
 	});
+	
+	$('.js-slick-sliderblok').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		dots: false,
+		centerMode: true,
+		fade: true,
+		autoplay: true,
+		swipeToSlide: true,
+		speed: 2000,
+		cssEase: 'cubic-bezier(.19,1,.22,1)'
+	});
+	
+
 	
 	$('.slick-slider-coaches').slick({
 		slidesToShow: 1
